e3f10d435c4e76953926a05f29c885ddc01ceff2
alex2097 <151250187@smail.nju.edu.cn>
2016-11-09 16:10:15 +0800

    test commit No.1 reason listed in the Demo1_6/main.cpp

--- Demo1_6/main.cpp
+++ Demo1_6/main.cpp
@@ -4,6 +4,7 @@
  * so the assembler faces the duplicated version
  *
  * seems the two .out files are the same(orz)
+ * try nm -C *.out |grep [funcname]|grep w again
  */
 #include <iostream>
 void call();
@@ -12,5 +13,8 @@ using namespace std;
 int main() {
     call();
     call1();
+    cout<<"the second test"<<endl;
+    cout<<"this test is for changing push author"<<endl;
+    cout<<"the author now is alex2097"<<endl;
     return 0;
 }
